0e696aadffce8d7349dd7918a4055941291cd365
Rethabile-C <rethabilechabisi@gmail.com>
2025-07-03 15:19:49 +0200

    Add files via upload

---
+++ Python/hello_world.py
@@ -0,0 +1,8 @@
+#Advanced concepts - strings
+
+message = 'Hello, World'
+
+print(len(message))
+print(message.strip()) #remove leading and tailing whitespace
+print(message.lower()) #convert all characters to small cases
+print (message.split(',')) #split the sstring into a list based on the comma
